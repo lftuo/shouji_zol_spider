3bc1b5bc17c34eb114bed4c46939be29801ede3c
lftuo <909709223@qq.com>
2017-11-03 17:38:25 +0800

    上传建表语句SQL文件

---
+++ shouji_zol_spider/create_table.sql
@@ -0,0 +1,26 @@
+-- auto-generated definition
+CREATE TABLE shouji_zol_spider_data
+(
+  id               VARCHAR(50)  NULL,
+  title            VARCHAR(200) NULL,
+  price            VARCHAR(20)  NULL,
+  screen_size      VARCHAR(50)  NULL,
+  screen_material  VARCHAR(100) NULL,
+  resolution       VARCHAR(100) NULL,
+  opreating_system TEXT         NULL,
+  cpu_name         VARCHAR(100) NULL,
+  core_nums        VARCHAR(50)  NULL,
+  ram              TEXT         NULL,
+  rom              TEXT         NULL,
+  phone_color      VARCHAR(100) NULL,
+  phone_material   TEXT         NULL,
+  sim              VARCHAR(100) NULL,
+  sim_max_nums     VARCHAR(50)  NULL,
+  battery          VARCHAR(50)  NULL,
+  type1            TEXT         NULL,
+  type2            TEXT         NULL,
+  model            TEXT         NULL,
+  time             VARCHAR(50)  NULL,
+  url              VARCHAR(100) NULL,
+  data_source      VARCHAR(100) NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
